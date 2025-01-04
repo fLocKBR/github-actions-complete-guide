6a938c88ee83266365992b1c370a5f9689983788
Wallison Barbosa <wallison.barbosa@4linux.com.br>
2025-01-04 12:37:36 -0300

    Add inputs and outputs for deploy-s3-javascript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  const bucketName = core.getInput('bucket-name', { required: true });
+  const sourceDir = core.getInput('source-dir', { required: true });
+  const region = core.getInput('region', { required: true });
+  const s3Uri = `s3://${bucketName}`;
+
+  exec.exec(`aws s3 sync ${sourceDir} ${s3Uri} --delete --region ${region}`);
+
+  const websiteUrl = `http://${bucketName}.s3-website-${region}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl);
+
   core.notice('Deploying to S3');
 }
 
